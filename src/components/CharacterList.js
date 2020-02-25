d032368687c1b718633ea2ee03146a1806e721f9
Seth Nelson <seth.nelson@outlook.com>
2020-02-25 15:51:43 -0800

    setup axios call for API info, filtered the query data to return lower case to match the query entry for search

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
-import SearchFOrm from './SearchForm';
+import SearchForm from './SearchForm';
 import CharacterCard from './CharacterCard';
 
 export default function CharacterList() {
@@ -8,10 +8,25 @@ export default function CharacterList() {
   const [ characterInfo, setCharacterInfo ] = useState([]);
   const [ searchQuery, setSearchQuery ] = useState('');
 
+  const handleInputChanges = event => {
+    setSearchQuery(event.target.value);
+  };
+
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    axios
+      .get('https://rickandmortyapi.com/api/character/')
+      .then(response => {
+        const characters = response.data.results.filter(character =>
+          character.name.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+          setCharacterInfo(characters);
+      })
+      .catch(error => {
+        console.log('Error!', error);
+      })
+  }, [searchQuery]);
 
   return (
     <section className="character-list">
