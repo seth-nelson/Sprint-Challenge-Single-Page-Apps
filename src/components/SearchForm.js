7c16ed5f26bfce5df6fddc8bfde3868c92e64096
Seth Nelson <seth.nelson@outlook.com>
2020-02-25 15:43:05 -0800

    setup searchform with searchquery and handle changes

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
 
-export default function SearchForm() {
+export default function SearchForm(props) {
  
   return (
     <section className="search-form">
-     // Add a search form here
+      <form>
+        <input
+          type='text'
+					onChange={props.handleInputChange}
+					value={props.searchQuery}
+          placeholder={props.placeHolder}
+        />
+      </form>
     </section>
   );
 }
