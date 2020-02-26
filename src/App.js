b3ec7a47c06df1f33a4466663e223b511ad69d8f
Seth Nelson <seth.nelson@outlook.com>
2020-02-25 16:25:19 -0800

    added photo for background

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import Header from "./components/Header.js";
 import { Route } from 'react-router-dom';
 import WelcomePage from './components/WelcomePage';
 import CharacterList from './components/CharacterList';
+import styled from 'styled-components';
+
+const StyledMain = styled.div`
+  background-color: darkGray;
+  text-align: center;
+`;
 
 export default function App() {
   return (
-    <main>
+    <StyledMain>
       <Header />
       <Route exact path='/' component={WelcomePage} />
       <Route path='/characters' component={CharacterList} />
-    </main>
+    </StyledMain>
   );
 }
