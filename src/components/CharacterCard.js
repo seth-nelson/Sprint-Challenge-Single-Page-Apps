e9c7d51ecb836d5faae7a13a12927eef296d8004
Seth Nelson <seth.nelson@outlook.com>
2020-02-25 16:15:50 -0800

    added styling via styled components to CharacterCard and CharacterList. also updated line breaks on html

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,13 +1,26 @@
 import React from "react";
+import styled from 'styled-components';
+
+const StyledDiv = styled.div` 
+  background-color: 008f11;
+  border-radius: 10px;
+  border: 2px solid 3c89d0;
+  text-align: center;
+`;
+
+const StyledImg = styled.img`
+  border-radius: 20px;
+  border: 3px solid #3c89d0; 
+`;
 
 export default function CharacterCard({ image, name, gender, species, status}) {
   return (
-    <div>
-      <img src={image} alt='rick'></img>
+    <StyledDiv>
+      <StyledImg src={image} alt='rick'></StyledImg>
       <h2>{name}</h2>
       <p>{gender}</p>
       <p>Species: {species}</p>
       <p>Status: {status}</p>
-    </div>
+    </StyledDiv>
   )
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,6 +2,13 @@ import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import SearchForm from './SearchForm';
 import CharacterCard from './CharacterCard';
+import styled from 'styled-components';
+
+const StyledList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
@@ -31,19 +38,21 @@ export default function CharacterList() {
   return (
     <div>
       <SearchForm placeHolder='Search Characters' searchQuery={searchQuery} handleInputChanges={handleInputChanges} />
-      <section className="character-list">
-        {characterInfo.map(character => {
-          return (
-            <CharacterCard
-              key={character.id}
-              image={character.image}
-              name={character.name}
-              species={character.species}
-              status={character.status}
-            />
-          );
-        })}
-      </section>
+        <section className="character-list">
+          {characterInfo.map(character => {
+            return (
+              <StyledList>
+                <CharacterCard
+                  key={character.id}
+                  image={character.image}
+                  name={character.name}
+                  species={character.species}
+                  status={character.status}
+                />
+              </StyledList>
+            );
+          })}
+        </section>
     </div>
   );
 }
