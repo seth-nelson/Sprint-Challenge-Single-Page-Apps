c3d0c8ab4cb2713792778e96dfadb727c636a6f3
Seth Nelson <seth.nelson@outlook.com>
2020-02-25 16:19:56 -0800

    added styling to searchform. more to come

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,7 +1,23 @@
 import React, { useState } from "react";
+import styled from 'styled-components'
+
+const StyledButton = styled.button`
+  background-color: black;
+  padding: 6px 10px;
+  width: 60px;
+ text-align: center;
+  margin: 5px;
+  border: 1px solid #00ff00;
+  border-radius: 3px;
+  color: #00ff00;
+  &hover {
+    background-color: #00ff00;
+    color: black;
+  }
+`;
 
 export default function SearchForm(props) {
- 
+
   return (
     <section className="search-form">
       <form>
@@ -11,6 +27,7 @@ export default function SearchForm(props) {
 					value={props.searchQuery}
           placeholder={props.placeHolder}
         />
+        <StyledButton type='submit'>Search</StyledButton>
       </form>
     </section>
   );
